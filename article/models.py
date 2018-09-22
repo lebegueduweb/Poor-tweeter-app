028463a8720aae604ecc1525b10e309546ceaede
lebegueduweb <38869605+lebegueduweb@users.noreply.github.com>
2018-09-22 18:29:16 +0300

    Update models.py
    changed the date model to display a naive date and time(GMT time)

--- article/models.py
+++ article/models.py
@@ -7,5 +7,5 @@ from django.utils import timezone
 class Article(models.Model):
     article_heading = models.CharField(max_length=200)
     article_body = models.TextField(max_length=50)
-    date = models.DateField(("Date"), default=datetime.date.today)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
     article_id = models.AutoField(primary_key=True)
